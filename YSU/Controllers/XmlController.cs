ee22973eeb61bf264a04c1200eeeea9fdc264049
Saima <skhan.usa21@gmail.com>
2023-11-16 22:08:12 -0500

    DataBase is created in mongoDB. Connection String is working.

--- YSU/Controllers/XmlController.cs
+++ YSU/Controllers/XmlController.cs
@@ -1,8 +1,9 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MongoDB.Driver;
+using System.Collections.Generic;
 using System.IO;
 using System.Xml.Serialization;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using YSU.Models;
 
 namespace YSU.Controllers
@@ -12,7 +13,6 @@ namespace YSU.Controllers
         private readonly MongoDbContext _dbContext;
         private readonly ILogger<XmlController> _logger;
 
-        // Constructor to inject the MongoDbContext and ILogger
         public XmlController(MongoDbContext dbContext, ILogger<XmlController> logger)
         {
             _dbContext = dbContext;
@@ -22,25 +22,27 @@ namespace YSU.Controllers
         [HttpPost]
         public IActionResult Post(Award award)
         {
-            // Insert the Award into the MongoDB collection
             _dbContext.Awards.InsertOne(award);
-
-            // Return Ok if the operation is successful
             return Ok();
         }
 
         [HttpGet]
         public IActionResult ReadXmlFiles()
         {
-            string folderPath = @"/Users/saimakhan/ETLProject/YSU/bin/Debug/net7.0/XML";
+            // Replace <username>, <password>, <cluster>, and <databaseName> with your actual MongoDB Atlas credentials
+            var connectionStringRegular = "mongodb+srv://skhanusa21:Canon700D@cluster0.pba3oyh.mongodb.net/NSF";
+            var dbContextRegular = new MongoDbContext(connectionStringRegular);
 
-            // Get all XML files in the specified folder
+            // Replace <username>, <password>, <cluster>, and <databaseName> with your actual MongoDB Atlas credentials
+            var connectionStringSCRAM = "mongodb+srv://skhanusa21:Canon700D@cluster0.pba3oyh.mongodb.net/NSF?authMechanism=SCRAM-SHA-1";
+            var dbContextSCRAM = new MongoDbContext(connectionStringSCRAM);
+
+            // Rest of your code for reading XML files and inserting into MongoDB
+            string folderPath = @"/Users/saimakhan/ETLProject/YSU/bin/Debug/net7.0/XML";
             string[] xmlFiles = Directory.GetFiles(folderPath, "*.xml");
 
-            // Create a list to store the deserialized objects
             List<RootTag> rootTags = new List<RootTag>();
 
-            // Deserialize each XML file
             foreach (string filePath in xmlFiles)
             {
                 using (FileStream fs = new FileStream(filePath, FileMode.Open))
@@ -48,15 +50,19 @@ namespace YSU.Controllers
                     XmlSerializer serializer = new XmlSerializer(typeof(RootTag));
                     RootTag rootTag = (RootTag)serializer.Deserialize(fs);
 
+                    // Insert each Award into MongoDB if it exists
+                    if (rootTag.Award != null)
+                    {
+                        dbContextRegular.Awards.InsertOne(rootTag.Award);
+                        dbContextSCRAM.Awards.InsertOne(rootTag.Award);
+                    }
+
+                    // Add the deserialized object to the list
                     rootTags.Add(rootTag);
                 }
             }
 
             return View(rootTags);
         }
-
     }
 }
-
-
-
